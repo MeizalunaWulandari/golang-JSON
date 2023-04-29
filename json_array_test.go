14b543eae2dc209e8c726e263244a2cc66caced8
Meizaluna Wulandari <MeizalunaWulandari@gmail.com>
2023-04-29 10:32:20 +0800

    JSON Array - Decode

--- json_array_test.go
+++ json_array_test.go
@@ -21,3 +21,32 @@ func TestJSONArray(t *testing.T) {
 	bytes, _ := json.Marshal(customer)
 	fmt.Println(string(bytes))
 }
+
+func TestJSONArrayDecode(t *testing.T) {
+	jsonString := `
+		{
+			"FirstName":"Meizaluna",
+			"LastName":"Wulandari",
+			"Age":16,
+			"Married":false,
+			"Hobbies":[
+				"Gaming",
+				"Reading",
+				"Coding",
+				"Sleeping"
+			]
+		}
+	`
+	jsonBytes := []byte(jsonString)
+	customer := &Customer{}
+	err := json.Unmarshal(jsonBytes, customer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(customer)
+	fmt.Println(customer.FirstName)
+	fmt.Println(customer.LastName)
+	fmt.Println(customer.Age)
+	fmt.Println(customer.Married)
+	fmt.Println(customer.Hobbies)
+}
