2f21d1273c05e8d4261332bdfa588b261d878b80
Meizaluna Wulandari <MeizalunaWulandari@gmail.com>
2023-04-29 09:51:11 +0800

    JSON Object

---
+++ json_object_test.go
@@ -0,0 +1,26 @@
+package golang_json
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+type Customer struct {
+	FirstName string
+	LastName  string
+	Age       int
+	Married   bool
+}
+
+func TestJSONObject(t *testing.T) {
+	customer := Customer{
+		FirstName: "Meizaluna",
+		LastName:  "Wulandari",
+		Age:       16,
+		Married:   false,
+	}
+
+	bytes, _ := json.Marshal(customer)
+	fmt.Println(string(bytes))
+}
